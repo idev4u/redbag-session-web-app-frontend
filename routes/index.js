99b6232df462f4269648a3ec82a3b2479ed0e579
Norman Sutorius <norman.sutorius@porsche.de>
2019-11-28 16:31:55 +0100

    Create the mvp of the app

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  // request to the backend
+  res.render('index', {
+    title: 'Walters quote of the day :-)',
+    quote: 'Wenn den Baum siehst, in den du rein fährst, hast untersteuern. Wenn ihn nur hörst, hast übersteuern'
+  });
 });
 
 module.exports = router;
